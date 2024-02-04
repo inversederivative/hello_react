7b7f0377197a6fac46bc6d25e928c8b4e9191801
John Hatton <hattonjd@iu.edu>
2024-02-05 00:36:11 +0100

    Change link to Github Repo in App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/inversederivative/hello_react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hello_react
         </a>
       </header>
     </div>
